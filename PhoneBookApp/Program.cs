e12873561a96e93dc8eae087dc46b3995b20eb07
Walter4B <walter.bali.markovi@gmail.com>
2022-05-03 20:23:13 +0200

    Add project files.

---
+++ PhoneBookApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ConsoleTableExt;
+using System.Configuration;
+using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace PhoneBookApp
+{
+    class Program
+    {
+        static void Main()
+        { 
+        
+        }
+    }
+}
